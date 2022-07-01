e13ab086fd13f42e0c59488f3702af110ff55d78
Stensith <78397691+Stensith@users.noreply.github.com>
2022-07-01 21:55:38 +0530

    added image cmd to elena_gui

--- elena_gui.py
+++ elena_gui.py
@@ -8,23 +8,30 @@ from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
-
+from kivy.config import Config
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
 import os
 
+# for opengl error
 os.environ["KIVY_GL_BACKEND"] = "angle_sdl2"
+
+Config.set("graphics", "resizable", True)
 
 
 class Elena(App):
     def build(self):
+
         return Label(
             text="Hello i Am Elena!!",
             color=[0.41, 0.42, 0.74, 1],
             font_size="60sp",
+            font_name="fonts/Roboto-Regular.ttf",
         )
 
+    def build(self):
+        return Image(source="elena.jpg")
 
-def elena_main_screen():
-    label = Elena()
-    label.run()
 
-elena_main_screen()
+if __name__ == "__main__":
+    Elena().run()
